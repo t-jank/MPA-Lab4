b8a1108edc4d1dabcf3f812265476282644f5b39
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-12-19 21:07:43 +0100

    random permutation generator by Fischer-Yates shuffle added :)

--- random_permutations.py
+++ random_permutations.py
@@ -4,11 +4,15 @@ Created on Mon Dec 19 17:25:20 2022
 
 @author: t-jan
 """
+import random
 
 def random_permutation(n):
     a=[]
     for i in range(0,n):
         a.append(i+1)
+    for i in range(0,n-1): # Fisher–Yates shuffle
+        j = random.randint(i,n-1)
+        a[i], a[j] = a[j], a[i]
     return a
 
 print(random_permutation(10))
