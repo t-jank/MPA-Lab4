bc96461540bb6580cbad980c258b86580a7b6c1f
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-12-27 14:48:23 +0100

    concentration bounds added, but chernoff not sure

--- random_permutations.py
+++ random_permutations.py
@@ -7,6 +7,7 @@ Created on Mon Dec 19 17:25:20 2022
 import random
 import math
 import matplotlib.pyplot as plt
+import statistics
 
 
 def random_permutation(n):
@@ -55,27 +56,54 @@ def cycles(permutation):
     return cycles_counter
 
 nMin=5
-nMax=10000
-nStep=10
-nRepeats=20
+nMax=100
+nStep=1
+nRepeats=1
+alfa=0.1 #0.1=90%
 #fp_big_sum=0
 draw=True
+results=[]
 for n in range(nMin,nMax,nStep):
-    sum=0
+    summ=0
     for r in range(0,nRepeats): 
         a = random_permutation(n)
         fp = fixed_points(a)
-       # cc = cycles(a)
+     #   cc = cycles(a)
      #   rc=records(a)
-        sum += fp
+        results.append(fp)
+        summ += fp
      #   fp_big_sum += fp
     if draw==True:
-        plt.scatter(n,sum/nRepeats,color='hotpink')
+        plt.scatter(n,summ/nRepeats,color='k')
 if draw==True:
     plt.xlim(left=0)
     plt.xlim(right=nMax)
     plt.xlabel('n')
     plt.ylabel('fixed points')
     plt.title('Liczba punktów stałych losowej permutacji')
-    plt.show()
 #print('Average number of fixed points:',fp_big_sum/(math.ceil((nMax-nMin)/nStep)*nRepeats))
+deltaczeb=math.sqrt(statistics.variance(results)/alfa)
+avgres=statistics.mean(results)
+yczeb1=2*[avgres+deltaczeb]
+yczeb2=2*[avgres-deltaczeb]
+xrange=[nMin,nMax-1]
+plt.plot(xrange,yczeb1,color='b',label='Czebyszew')
+plt.plot(xrange,yczeb2,color='b')
+pomrzecz=results
+for i in range(0,len(pomrzecz)):
+    pomrzecz[i]=abs(pomrzecz[i]-avgres)
+pomrzecz.sort()
+deltarzecz=pomrzecz[math.ceil((1-alfa)*len(pomrzecz))]
+yrzecz1=2*[avgres+deltarzecz]
+yrzecz2=2*[avgres-deltarzecz]
+plt.plot(xrange,yrzecz1,color='limegreen',label='Rzeczywistość')
+plt.plot(xrange,yrzecz2,color='limegreen')
+deltachern=3.31621889700067 # ex=1, alfa=0.1
+ychern1=2*[avgres+deltachern]
+ychern2=2*[avgres-deltachern]
+plt.plot(xrange,ychern1,color='hotpink',label='Chernoff')
+plt.plot(xrange,ychern2,color='hotpink')
+plt.legend()
+plt.show()
+print(deltarzecz)
+print(avgres)
